5edb8bc7623b874a60460c8242e2c3eb240cbe65
Zohra Ilhom <zohra.ilhom@gmail.com>
2020-07-11 11:10:23 -0400

    testing

---
+++ src/ProTesting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function testing() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>Testing</p>
+        </div>
+    )
+}
